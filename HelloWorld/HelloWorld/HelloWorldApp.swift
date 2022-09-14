8ba978be7d4ff8f350443bc27f571be06d0fd849
Dementev Valerii <box730@mail.ru>
2022-09-14 22:28:14 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Валерий Дементьев on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
